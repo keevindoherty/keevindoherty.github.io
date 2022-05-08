6850eae29b515e1e53cdbf71d659a41cd6ae633e
Kevin <kdoherty@mit.edu>
2022-05-08 18:57:23 -0400

    Migrate new template.

---
+++ _bibliography/auto/papers.el
@@ -0,0 +1,22 @@
+(TeX-add-style-hook
+ "papers"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "pearson2022improving"
+    "rosen2021advances"
+    "doherty2019learning"
+    "dohertySparseSupplement"
+    "lu2022slam"
+    "lu2021consensus"
+    "fu2021multi"
+    "martin2020variational"
+    "doherty2020probabilistic"
+    "doherty2019multimodal"
+    "doherty2018approximate"
+    "shan2018bayesian"
+    "doherty2017bayesian"
+    "doherty2016probabilistic"
+    "bai2015inference"
+    "doherty2022performance"))
+ :bibtex)
+
